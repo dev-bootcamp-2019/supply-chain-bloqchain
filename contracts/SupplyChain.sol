6cc5e57b15ca730f3803bf2583d59c2679359fa8
Bloqchain <bloqchain@gmail.com>
2019-01-25 11:41:56 -0800

    Add files via upload

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -12,10 +12,12 @@ contract SupplyChain {
   address owner;
 
   /* Add a variable called skuCount to track the most recent sku # */
+  uint public skuCount;
 
   /* Add a line that creates a public mapping that maps the SKU (a number) to an Item.
      Call this mappings items
   */
+  mapping (uint => Item) public items;
 
   /* Add a line that creates an enum called State. This should have 4 states
     ForSale
@@ -24,23 +26,47 @@ contract SupplyChain {
     Received
     (declaring them in this order is important for testing)
   */
+  enum State { ForSale, Sold, Shipped, Received }
 
   /* Create a struct named Item.
     Here, add a name, sku, price, state, seller, and buyer
     We've left you to figure out what the appropriate types are,
     if you need help you can ask around :)
   */
+  struct Item {
+    string name;
+    uint sku;
+    uint price;
+    State state;
+    address payable seller;
+    address payable buyer;
+  }
 
   /* Create 4 events with the same name as each possible State (see above)
     Each event should accept one argument, the sku*/
+  event ForSale(
+    uint sku
+  );
+
+  event Sold(
+    uint sku
+  );
+
+  event Shipped(
+    uint sku
+  );
+  
+  event Received(
+    uint sku
+  );
 
 /* Create a modifer that checks if the msg.sender is the owner of the contract */
 
-  modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
-
+  modifier verifyCaller (address payable _address) { require (msg.sender == _address); _;}
+  modifier verifyOwner () { require (msg.sender == owner); _;}
   modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
   modifier checkValue(uint _sku) {
-    //refund them after pay for item (why it is before, _ checks for logic before func)
+    // refund them after pay for item (why it is before, _ checks for logic before func)
     _;
     uint _price = items[_sku].price;
     uint amountToRefund = msg.value - _price;
@@ -50,20 +76,24 @@ contract SupplyChain {
   /* For each of the following modifiers, use what you learned about modifiers
    to give them functionality. For example, the forSale modifier should require
    that the item with the given sku has the state ForSale. */
-  modifier forSale
-  modifier sold
-  modifier shipped
-  modifier received
+  modifier forSale (Item memory _item) { require (_item.state == State.ForSale, "Item not for sale"); _;}
+  modifier sold (Item memory _item) { require (_item.state == State.Sold, "Item not sold"); _;}
+  modifier shipped (Item memory _item) { require (_item.state == State.Shipped, "Item not shipped"); _;}
+  modifier received (Item memory _item) { require (_item.state == State.Received, "Item not received"); _;}
 
 
   constructor() public {
     /* Here, set the owner as the person who instantiated the contract
        and set your skuCount to 0. */
+       owner = msg.sender;
+       skuCount = 0;
   }
+
+  // CW, I put in msg.sender into buyer below.... which makes no sense.... but it got it to compile
 
   function addItem(string memory _name, uint _price) public returns(bool){
     emit ForSale(skuCount);
-    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
+    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: 0x0000000000000000000000000000000000000000});
     skuCount = skuCount + 1;
     return true;
   }
@@ -75,23 +105,40 @@ contract SupplyChain {
     refunded any excess ether sent. Remember to call the event associated with this function!*/
 
   function buyItem(uint sku)
-    public
-  {}
+    public payable forSale(items[sku]) paidEnough(items[sku].price) checkValue(sku)
+  {
+    //transfer money to seller
+    items[sku].seller.transfer(items[sku].price);
+    //Set buyer as message sender
+    items[sku].buyer = msg.sender;
+    //Set state to sold
+    items[sku].state = State.Sold;
+    emit Sold(sku);
+
+  }
 
   /* Add 2 modifiers to check if the item is sold already, and that the person calling this function
   is the seller. Change the state of the item to shipped. Remember to call the event associated with this function!*/
   function shipItem(uint sku)
-    public
-  {}
+    public sold(items[sku]) verifyCaller(items[sku].seller)
+  {
+    //change state of item to shipped
+    items[sku].state = State.Shipped;
+    emit Shipped(sku);
+  }
 
   /* Add 2 modifiers to check if the item is shipped already, and that the person calling this function
   is the buyer. Change the state of the item to received. Remember to call the event associated with this function!*/
   function receiveItem(uint sku)
-    public
-  {}
+    public shipped(items[sku]) verifyCaller(items[sku].buyer)
+  {
+    //change state of item to received
+    items[sku].state = State.Received;
+    emit Received(sku);
+  }
 
   /* We have these functions completed so we can run tests, just ignore it :) */
-  function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, uint state, address seller, address buyer) {
+  function fetchItem(uint _sku) public view returns (string memory name, uint sku, uint price, uint state, address seller, address buyer) {
     name = items[_sku].name;
     sku = items[_sku].sku;
     price = items[_sku].price;
--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -6,10 +6,93 @@ import "../contracts/SupplyChain.sol";
 
 contract TestSupplyChain {
 
-    // Test for failing conditions in this contracts
+    function testConstructor () public {
+
+        uint expected_sku = 0;
+
+        Assert.equal(supplyChain.skuCount(), expected_sku, "Sku count is not initialized to 0");
+    }
+
+    //State Variables
+    SupplyChain supplyChain;
+    Seller seller;
+    Buyer buyer;
+    uint skuCount;
+    uint price;
+    uint state;
+    uint public initialBalance = 10 ether;
+    //store return values
+    string a;
+    uint b;
+    uint c;
+    uint d;
+    address e;
+    address f;
+
+    function beforeAll () public {
+        //Deploy SupplyChain, Seller and Buyer contracts
+        supplyChain = new SupplyChain();
+        seller = new Seller();
+        buyer = new Buyer();
+        address(buyer).transfer(10 ether);
+    }
+    
+    function testAddItem () public {
+        
+        string memory name = "tuxedo t-shirt";
+        price = 1 ether;
+        skuCount = 0;
+        state = 0;
+        address itemSeller = address(seller);
+        address itemBuyer = 0x0000000000000000000000000000000000000000;
+
+        //Have seller contract add item
+        seller.addItem2supply(supplyChain, name, price);
+        
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
+
+        Assert.equal(a, name, "Name of last added item does not match expected value");
+        Assert.equal(b, skuCount, "SKU count of last added item does not match expected value");
+        Assert.equal(c, price, "Price of last added item does not match expected value");
+        Assert.equal(d, state, "State of last added item does not match expected value");
+        Assert.equal(e, itemSeller, "Seller of last added item does not match expected value");
+        Assert.equal(f, itemBuyer, "Buyer of last added item is not 0");
+
+
+    }
+
+    // Test for failing conditions in this contract
     // test that every modifier is working
 
     // buyItem
+    
+    function testBuyItem () public {
+
+        state = 1;
+        //Store account balances before transactions
+        //Seller
+        uint sellerPreBalance = address(seller).balance;
+        //Buyer
+        uint buyerPreBalance = address(buyer).balance;
+
+        //Have buyer contract buy item
+        buyer.buyItem(supplyChain, skuCount);
+        
+        //Store account balances after transactions
+        //Seller
+        uint sellerPostBalance = address(seller).balance;
+        //Buyer
+        uint buyerPostBalance = address(buyer).balance;
+        
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
+
+        Assert.equal(d, state, "State of last added item does not match expected value");
+        Assert.equal(f, address(buyer), "Buyer of last added item is not 0");
+        Assert.equal(sellerPostBalance, sellerPreBalance + c, "Seller's balance should increase by price of item");
+        //Why isn't this "isBelow"
+        Assert.equal(buyerPostBalance, buyerPreBalance - c, "Buyer's balance should increase by more than price of item");
+    }
+    
 
     // test for failure if user does not send enough funds
     // test for purchasing an item that is not for Sale
@@ -17,15 +100,71 @@ contract TestSupplyChain {
 
     // shipItem
 
+    function testShipItem () public {
+        state = 2;
+
+        //Have seller ship item
+        seller.shipItem(supplyChain, skuCount);
+
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
+
+        Assert.equal(d, state, "State of Item should be Shipped");
+    }
+
     // test for calls that are made by not the seller
     // test for trying to ship an item that is not marked Sold
 
     // receiveItem
 
+    function testReceiveItem () public {
+        state = 3;
+
+        //Have buyer mark item as received
+        buyer.receiveItem(supplyChain, skuCount);
+
+        (a, b, c, d, e, f) = supplyChain.fetchItem(skuCount);
+
+        Assert.equal(d, state, "State of Item should be Shipped");
+    }
+
     // test calling the function from an address that is not the buyer
     // test calling the function on an item not marked Shipped
+}
 
-     
+contract Seller {
 
+    function addItem2supply(SupplyChain _supplyChain, string memory _item, uint _price) public returns (bool) {
+        
+        return _supplyChain.addItem(_item, _price);
+    }
+
+    function shipItem(SupplyChain _supplyChain, uint _sku) public {
+        
+        _supplyChain.shipItem(_sku);
+    }
+
+    function() external payable {
+
+    }
+}
+
+contract Buyer {
+
+    function buyItem(SupplyChain _supplyChain, uint _sku) public {
+
+        uint amount = 2 ether;
+        
+        _supplyChain.buyItem.value(amount)(_sku);
+
+    }
+
+    function receiveItem(SupplyChain _supplyChain, uint _sku) public {
+
+        _supplyChain.receiveItem(_sku);
+    }
+
+    function() external payable {
+
+    }
 
 }
